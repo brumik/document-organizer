3d36d3fbc3efcaca9ce4993a23e7406f3118e299
Levente Berky <levente.berky@gmail.com>
2022-05-12 15:58:24 +0200

    Added a notification on startup if expiry is upcoming

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import Main from './main/mainWindow';
-import { ipcMain, Notification } from 'electron';
+import { ipcMain } from 'electron';
 import settings from './main/IPC/settings';
 import database from './main/IPC/database';
 import ProjectStore from './main/Store/projects';
 import DocumentStore from './main/Store/documents';
 import PreferencesStore from './main/Store/preferences';
+import notificationCheckup from './main/tasks/notificationCheckup';
 
 global.preferencesStore = new PreferencesStore({
   configName: 'user-preferences',
@@ -25,4 +26,6 @@ const main = new Main();
 main.onEvent.on("window-created", () => {
   settings.initIpcMain(ipcMain, main.window);
   database.initIpcMain(ipcMain, main.window);
+
+  notificationCheckup();
 });
